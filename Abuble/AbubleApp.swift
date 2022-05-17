2e0379b49455a7c745c1fdc19a3a212ef92da581
larissapaschoalin <larissa.paschoalin@hotmail.com>
2022-05-17 07:27:30 -0300

    Initial Commit

---
+++ Abuble/AbubleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbubleApp.swift
+//  Abuble
+//
+//  Created by Larissa Paschoalin on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AbubleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Abuble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Abuble
+//
+//  Created by Larissa Paschoalin on 17/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
