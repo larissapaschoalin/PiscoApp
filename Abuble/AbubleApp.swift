b9a725f13beba3f049870110ce0a999a75c1a875
larissapaschoalin <larissa.paschoalin@hotmail.com>
2022-05-20 15:23:49 -0300

    Novo endereço

--- Abuble/AbubleApp.swift
+++ Abuble/AbubleApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import GameKit
 
 @main
 struct AbubleApp: App {
+    @StateObject var user = User()
+    
+    func authenticateUser() {
+        let localPlayer = GKLocalPlayer.local
+        localPlayer.authenticateHandler = { vc, error in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            user.from(gameCenter: localPlayer)
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(user)
+                .onAppear {
+                    authenticateUser()
+                }
         }
     }
 }
